2196f5fb870cf9f87e16d4d492eaff8cf345ff5e
Dmytro Honchar <demetrgonchar@gmail.com>
2023-11-10 10:44:30 +0200

    modified nvim

--- nvim/lua/mercury/core/colorscheme.lua
+++ nvim/lua/mercury/core/colorscheme.lua
@@ -1,6 +1,9 @@
-local status, _ = pcall(vim.cmd, "colorscheme rose-pine")
+function ColorMy(color)
+	color = color or "rose-pine"
+	vim.cmd.colorscheme(color)
 
-if not status then
-	print("Colorscheme not found.")
-	return
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
+
+ColorMy()
